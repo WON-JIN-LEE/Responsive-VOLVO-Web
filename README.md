733d5352f8e831845d21fc6ca7915b18057442c5
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-14 00:20:26 +0900

    Create README.md, Add demo

---
+++ README.md
@@ -0,0 +1,19 @@
+# Responsive VOLVO Web
+
+### Project
+
+- 개요 : Responsive Landing Web으로 제작하기
+- 언어 : HTML, CSS, JavaScript
+- 기간 : 21.08.13
+
+### VOLVO Web [Click here](https://won-jin-lee.github.io/Responsive-VOLVO-Web)
+
+## Wide Screen
+
+![wide](demo/wide.PNG)
+
+## Small Screen
+
+|                          |                        |
+| ------------------------ | ---------------------- |
+| ![small](demo/small.PNG) | ![small](demo/nav.PNG) |
--- js/main.js
+++ js/main.js
@@ -13,15 +13,3 @@ const showMenu = (toggleId, navId) => {
 showMenu('#nav-toggle', '#nav-menu');
 
 
-//GSAP
-
-/*------- ANIMATION -------*/
-
-/*TITLE Y IMG*/
-gsap.from('.home_title', { opacity: -1, duration: 1.5, delay: 1 });
-gsap.from('.home__img', { opacity: 0, duration: 1, delay: 1, x: 100 });
-gsap.from('.home__information', { opacity: 0, duration: 1, delay: 1.2, y: -30 });
-
-/*BOX*/
-gsap.from('.box__medium', { opacity: 0, duration: 1.5, delay: 1.3, y: -50 });
-gsap.from('.box__small', { opacity: -1, duration: 1.5, delay: 1.4, y: -50 });
